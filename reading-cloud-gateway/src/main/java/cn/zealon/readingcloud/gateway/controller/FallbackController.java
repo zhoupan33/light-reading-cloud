53162361c2810ca375e51e2e2677edcae6664f9f
zhoupan <1427897430@qq.com>
2026-09-22 04:50:33 +0800

    Update FallbackController.java

--- reading-cloud-gateway/src/main/java/cn/zealon/readingcloud/gateway/controller/FallbackController.java
+++ reading-cloud-gateway/src/main/java/cn/zealon/readingcloud/gateway/controller/FallbackController.java
@@ -14,7 +14,7 @@ public class FallbackController {
     @GetMapping("/fallback")
     public Result fallback() {
         System.out.println("111");
-        System.out.println("9864");
+        System.out.println("2222");
         System.out.println("33333");
         System.out.println("4444");
         return ResultUtil.fail();
