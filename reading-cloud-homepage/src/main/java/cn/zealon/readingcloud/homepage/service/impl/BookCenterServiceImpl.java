b48c6a243dedd04cb7a9010034a44193b74bee22
zhoupan <1427897430@qq.com>
2026-09-23 20:59:32 +0800

    Update BookCenterServiceImpl.java

--- reading-cloud-homepage/src/main/java/cn/zealon/readingcloud/homepage/service/impl/BookCenterServiceImpl.java
+++ reading-cloud-homepage/src/main/java/cn/zealon/readingcloud/homepage/service/impl/BookCenterServiceImpl.java
@@ -25,10 +25,11 @@ public class BookCenterServiceImpl implements BookCenterService {
 
     @Override
     public Book getBookById(String bookId) {
-        System.out.println("ceshi11");
+        System.out.println("以我为准");
         String key = RedisBookKey.BookCenter.getFeignClientBookKey(bookId);
         System.out.println("ceshi22");
         System.out.println("以我为准33");
+        System.out.println("ceshi22");
         Book book = this.redisService.getCache(key, Book.class);
         if (book != null) {
             return book;
